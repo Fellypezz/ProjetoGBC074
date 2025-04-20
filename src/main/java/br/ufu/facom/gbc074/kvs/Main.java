00c7773168066d6f0591945c7498fe90f6a40059
Valter Fellype Ferreira Netto <fellype_netto@hotmail.com>
2025-04-20 19:09:55 -0300

    correcao na main

--- src/main/java/br/ufu/facom/gbc074/kvs/Main.java
+++ src/main/java/br/ufu/facom/gbc074/kvs/Main.java
@@ -1,40 +1,47 @@
 package br.ufu.facom.gbc074.kvs;
 
-import org.eclipse.paho.client.mqttv3.MqttException;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import java.io.IOException;
 import java.util.logging.Logger;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class Main {
-    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+    private static final Logger LOG = Logger.getLogger("Logger");
 
-    public static void main(String[] args) throws InterruptedException, IOException {
-        LOG.info("Iniciando servidor gRPC KVS...");
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Uso: java br.ufu.facom.gbc074.kvs.Main <porta>");
+            return;
+        }
+
+        int porta = Integer.parseInt(args[0]);
+
+        LOG.info("Iniciando servidor gRPC KVS na porta " + porta + "...");
 
         try {
-            // Cria e inicia o serviço KVS na porta 50051
-            Server server = ServerBuilder.forPort(50051)
-                    .addService(new KVSService())
+            KVSService servico = new KVSService(); // pode lançar MqttException
+
+            Server server = ServerBuilder
+                    .forPort(porta)
+                    .addService(servico)
                     .build();
 
             server.start();
-            LOG.info("Servidor KVS iniciado na porta 50051!");
+            LOG.info("Servidor KVS iniciado na porta " + porta + "!");
 
-            // Adiciona hook para desligamento gracioso
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
                 LOG.warning("Recebido sinal para desligar o servidor...");
                 server.shutdown();
                 LOG.info("Servidor KVS desligado com sucesso!");
             }));
 
-            // Mantém o servidor rodando
             server.awaitTermination();
         } catch (MqttException e) {
             LOG.severe("Erro ao iniciar o serviço MQTT: " + e.getMessage());
             e.printStackTrace();
-        } catch (Exception e) {
-            LOG.severe("Erro inesperado: " + e.getMessage());
+        } catch (IOException | InterruptedException e) {
+            LOG.severe("Erro ao iniciar o servidor gRPC: " + e.getMessage());
             e.printStackTrace();
         }
     }
